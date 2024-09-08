02a30c7136dfa98a94a668f8f6d28cfa3e81eff8
dwen168 <wjian2008@hotmail.com>
2024-09-08 14:18:01 +1000

    bulk commit
    env, code, etc

--- dags/stock_market.py
+++ dags/stock_market.py
@@ -4,10 +4,13 @@ from airflow.hooks.base import BaseHook
 from airflow.sensors.base import PokeReturnValue
 from airflow.operators.python import PythonOperator
 from airflow.providers.docker.operators.docker import DockerOperator
+from astro import sql as aql
+from astro.files import File
+from astro.sql.table import Table, Metadata
 from datetime import datetime
 import requests
 
-from include.stock_market.tasks import _get_stock_prices,_store_prices
+from include.stock_market.tasks import _get_stock_prices, _store_prices, _get_formatted_csv, BUCKET_NAME
 
 SYMBOL = 'AAPL'
 
@@ -61,7 +64,25 @@ def stock_market():
 
     )
 
+    get_formatted_csv = PythonOperator(
+        task_id = 'get_formatted_csv',
+        python_callable = _get_formatted_csv,
+        op_kwargs = {
+            'path': '{{ task_instance.xcom_pull(task_ids="store_prices") }}'
+        }
+    )
 
-    is_api_available() >> get_stock_prices >> store_prices >> format_prices
+    load_to_dw = aql.load_file(
+        task_id = 'load_to_dw',
+        input_file = File(path=f's3://{BUCKET_NAME}'+'/'+'{{ task_instance.xcom_pull(task_ids="get_formatted_csv") }}', conn_id='minio'),
+        output_table = Table(
+            name = 'stock_market',
+            conn_id = 'postgres',
+            metadata=Metadata(schema='public')
+        )
+    )
+
+
+    is_api_available() >> get_stock_prices >> store_prices >> format_prices >> get_formatted_csv >> load_to_dw
 
 stock_market()
--- include/stock_market/tasks.py
+++ include/stock_market/tasks.py
@@ -1,8 +1,22 @@
 from airflow.hooks.base import BaseHook
+from airflow.exceptions import AirflowNotFoundException
 import requests
 import json
 from minio import Minio
 from io import BytesIO
+
+BUCKET_NAME = 'stock-market'
+
+def _get_minio_client():
+    minio = BaseHook.get_connection('minio')
+    print(minio)
+    client = Minio(
+        endpoint=minio.extra_dejson['endpoint_url'].split('//')[1],
+        access_key=minio.login,
+        secret_key=minio.password,
+        secure=False
+    )
+    return client
 
 
 def _get_stock_prices(url, symbol):
@@ -13,29 +27,26 @@ def _get_stock_prices(url, symbol):
 
 
 def _store_prices(stock):
-    #minio = BaseHook.get_connection('minio').extra_dejson
-    minio = BaseHook.get_connection('minio')
-    print(minio)
-    client = Minio(
-        # after print, these two attributes could not be gotten, change to login and password
-        #endpoint=minio['endpoint_url'].split('//')[1],
-        #access_key=minio['aws_access_key_id'],
-        #secret_key=minio['aws_secret_access_key'],
-        endpoint=minio.extra_dejson['endpoint_url'].split('//')[1],
-        access_key=minio.login,
-        secret_key=minio.password,
-        secure=False
-    )
+    client = _get_minio_client()
     bucket_name = 'stock-market'
-    if not client.bucket_exists(bucket_name):
-        client.make_bucket(bucket_name)
+    if not client.bucket_exists(BUCKET_NAME):
+        client.make_bucket(BUCKET_NAME)
     stock = json.loads(stock)
     symbol = stock['meta']['symbol']
     data = json.dumps(stock, ensure_ascii=False).encode('utf8')
     objw = client.put_object(
-        bucket_name=bucket_name,
+        bucket_name=BUCKET_NAME,
         object_name=f'{symbol}/prices.json',
         data=BytesIO(data),
         length=len(data)
     )
     return f'{objw.bucket_name}/{symbol}'
+
+def _get_formatted_csv(path):
+    client = _get_minio_client()
+    prefix_name = f"{path.split('/')[1]}/formatted_prices/"
+    objects = client.list_objects(BUCKET_NAME, prefix=prefix_name, recursive=True)
+    for obj in objects:
+        if obj.object_name.endswith('.csv'):
+            return obj.object_name
+    raise AirflowNotFoundException('The csv file does not exist')
